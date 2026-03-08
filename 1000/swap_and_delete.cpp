8c9538fb79aecc2df1d7eddbc85c0c1f40b233bb
its-me-meax <ultragaming123q@gmail.com>
2026-03-08 15:44:51 +0530

    done finally

---
+++ 1000/swap_and_delete.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int count0 = 0, count1 = 0;
+        // by obsercation
+        // count zeros and ones
+        for (char c : s) {
+            if (c == '0') count0++;
+            else count1++;
+        }
+
+        // minimum deletions needed we need to make all characters same is the absolute difference between count of zeros and ones
+        
+        int length_of_t = 0;
+        for (int i = 0; i < s.length(); i++) // n
+        {
+            if (s[i] == '0' && count1 > 0)
+                count1--, length_of_t++;
+            else if (s[i] == '1' && count0 > 0)
+                count0--, length_of_t++;
+            else
+                break;
+        }
+
+        cout << s.length() - length_of_t << endl;
+    }
+    return 0;
+}
