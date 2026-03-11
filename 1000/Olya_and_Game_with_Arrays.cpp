09e8e0ea8034158592bdf603c6ff0f09d1e6409b
its-me-meax <ultragaming123q@gmail.com>
2026-03-11 08:51:26 +0530

    fixed ssome errors

--- 1000/Olya_and_Game_with_Arrays.cpp
+++ 1000/Olya_and_Game_with_Arrays.cpp
@@ -6,34 +6,38 @@ int main(){
     cin.tie(NULL);
 
     int t;
+    cin >> t;
+
     while(t--){
-        //number of arrays in the list
         int n;
         cin >> n;
-       
-        vector<long long> second_elements;
 
+        vector<long long> second_elements;
         long long lowest_first_element = LLONG_MAX;
 
-        for(int  i= 0;i<n;i++){
-            long long m;
+        for(int i = 0; i < n; i++){
+            int m;
             cin >> m;
+
             vector<long long> a(m);
-            for(auto &x :a){
+            for(auto &x : a){
                 cin >> x;
             }
 
-            sort(a.begin(),a.end());
-            second_elements.push_back(a[1]);
+            sort(a.begin(), a.end());
+
             lowest_first_element = min(lowest_first_element, a[0]);
+            second_elements.push_back(a[1]);
         }
-        sort(second_elements.begin(),second_elements.end());
-        long long sum_of_second_elements = accumulate(second_elements.begin(),second_elements.end(),0LL);
 
-        long long answer = lowest_first_element + sum_of_second_elements-lowest_first_element;
-        cout << answer << endl;
+        sort(second_elements.begin(), second_elements.end());
 
+        long long sum = accumulate(second_elements.begin(), second_elements.end(), 0LL);
 
+        long long answer = lowest_first_element + sum - second_elements[0];
+
+        cout << answer << "\n";
     }
+
     return 0;
 }
