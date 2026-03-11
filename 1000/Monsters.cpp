079647cd11c9537778ea37dfb2e0ebfc8824e175
its-me-meax <ultragaming123q@gmail.com>
2026-03-11 21:31:18 +0530

    done

--- 1000/Monsters.cpp
+++ 1000/Monsters.cpp
@@ -6,36 +6,38 @@ int main(){
     cin.tie(NULL);
 
     int t;
-    cin  >> t;
+    cin >> t;
+
     while(t--){
         long long n,k;
-        cin >> n>>k;
-    // creating a map that gonna store the remainder or we can say that the health after the maximum attacks
+        cin >> n >> k;
 
-    unordered_map<long long,vector<int>> mp;
+        unordered_map<long long,vector<int>> mp;
 
-    for(int i =1;i<=n;i++){
-        long long a;
-        cin >>a;
-         long long r = a % k;
-         if(r==0) r=k;
-         // ow i have to store the reaminder and the coresspondng the indcies 
-         mp[r].push_back(i);
-    }
-    // prinri g orde rhigesht first then the lowest
-    for(long long r=k;r>=1;r--){
-        if(mp.count(r)){
-            for(int i=mp[r].size()-1;i>=0;i--){
-                cout << mp[r][i] << " ";
-            }
+        for(int i=1;i<=n;i++){
+            long long a;
+            cin >> a;
+
+            long long r = a % k;
+            if(r==0) r=k;
+
+            mp[r].push_back(i);
         }
+
+        vector<long long> keys;
+
+        for(auto &p : mp)
+            keys.push_back(p.first);
+
+        sort(keys.begin(),keys.end(),greater<long long>());
+
+        for(long long r : keys){
+            for(int idx : mp[r])
+                cout << idx << " ";
+        }
+
+        cout << "\n";
     }
 
-    cout << "\n";
-
-
-    }
     return 0;
 }
-
-
---
+++ 1000/ski_resort.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    while(t--){
+        // taking input of the length of array a , the minumum dys at resort and maximum confortable temprature
+        long long n,k,q;
+
+        vector<long long> a(n);
+        for(int i = 0;i<n;i++){
+            cin>>a[i];
+        }
+        vector<long long> b;
+
+
+        for(int i=0;i<n;i++){
+            if(a[i] < q){
+                b.push_back(i+1);
+                 }
+                 // now i got the psoiible days on whihc i can go to vacati
+
+        }
+
+    // now to find the number ways i can go 
+    
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
