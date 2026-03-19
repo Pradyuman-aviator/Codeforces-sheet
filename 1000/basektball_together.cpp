0c503986afcc7e07f720f0f02a9f2abe9ba1a976
its-me-meax <ultragaming123q@gmail.com>
2026-03-19 19:32:10 +0530

    done

--- 1000/basektball_together.cpp
+++ 1000/basektball_together.cpp
@@ -13,15 +13,44 @@ int main()
         int N,D;
         cin >> N >> D;
 
-        vector<int> power(N);
-        for(auto &x : power){
-                cin >> x;
-            }
+        vector<long long> power(N);
+        for(int i = 0;i<N;i++){
+            cin >> power[i];
+        }
 
            sort(power.begin(),power.end());
-            
-        
 
+           ///applying the bindayr search
+           
+           int left = -1;
+           int right = N-1;
+           int teams = 0;
+           int teamsize =1;
+
+           while(left<=right){
+            if(power[right]*teamsize >= D && left < right){
+                left++;
+                teamsize++;
+
+            }
+            else{
+                teams++;
+                right--;
+                teamsize = 1;
+
+
+            }
+           }
+
+
+
+
+
+
+        
+           cout << teams << "\n";
 
     }
+    return 0;
+    
 }
