02ad862b0c82eebfd546f4c252ae0effa7cf13d9
its-me-meax <ultragaming123q@gmail.com>
2026-08-14 13:14:19 +0530

    done

--- 1100/truck_boxes.cpp
+++ 1100/truck_boxes.cpp
@@ -1,31 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int t;
     cin >> t;
+
     while (t--)
     {
-
         int n;
-
         cin >> n;
 
-        vector<int> a(n);
+        vector<long long> a(n);
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++)
             cin >> a[i];
 
+        long long ans = 0;
+
+        for (int k = 1; k <= n; k++)
+        {
+            if (n % k == 0)
+            {
+                long long maxi = LLONG_MIN;
+                long long mini = LLONG_MAX;
+
+                for (int i = 0; i < n; i += k)
+                {
+                    long long sum = 0;
+
+                    for (int j = i; j < i + k; j++)
+                    {
+                        sum += a[j];
+                    }
+
+                    maxi = max(maxi, sum);
+                    mini = min(mini, sum);
+                }
+
+                ans = max(ans, maxi - mini);
+            }
         }
 
-        sort(a.rbegin(),a.rend());
-
-        int i = a[0];
-
-        int j = a[n-1];
-
-
-        cout << abs(i-j) << endl;
+        cout << ans << '\n';
     }
+
     return 0;
 }
